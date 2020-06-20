62d94489897d483cecb05cb0922615a8346349eb
Administrator <chestnutTang@163.com>
2020-06-20 16:59:17 +0800

    去掉actionbar

--- app/src/main/java/com/example/business/MainActivity.kt
+++ app/src/main/java/com/example/business/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
